75df059dc6e2f2eee491c9602ce6216832e1a4f3
charboneaut <charboneau@protonmail.ch>
2022-02-17 16:02:06 -0500

    proto rate limit for log calls

--- pages/components/updateLog/UpdateLog.js
+++ pages/components/updateLog/UpdateLog.js
@@ -2,8 +2,15 @@ import axios from "axios";
 import { v4 } from "uuid";
 import { useState } from "react";
 import styles from "./UpdateLog.module.css";
+import rateLimit from "axios-rate-limit";
 
 export default function UpdateLog(props) {
+  const [dont, setDont] = useState(false);
+  const http = rateLimit(axios.create(), {
+    maxRequests: 0.1,
+    perMilliseconds: 5000,
+    maxRPS: 1,
+  });
   let filler = [];
   if (props.data) {
     filler = props.data;
@@ -23,14 +30,19 @@ export default function UpdateLog(props) {
         }
       })}
       <button
+        disabled={dont}
         onClick={async () => {
-          const data = await axios.get(
+          const data = await http.get(
             "https://api.github.com/repos/charboneaut/xponlive/commits"
           );
           setMsgs(data.data);
           let newWindows = props.windows;
           newWindows["Update Log"].data = data.data;
           props.setWindows(newWindows);
+          setDont(true);
+          setTimeout(() => {
+            setDont(false);
+          }, 5000);
         }}
       >
         Fetch
